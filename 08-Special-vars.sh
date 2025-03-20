5bc961e2941ec233626c24ad2b6b7f7633bb3ad5
Sowmya Kancharla <vippasowmya0408@gmail.com>
2025-03-20 19:57:57 +0530

    Conditions script

--- 08-Special-vars.sh
+++ 08-Special-vars.sh
@@ -3,10 +3,9 @@
 echo "All variables passed: $@"
 echo "Number of variables: $#"
 echo "script name: $0"
-sleep 60 &
 echo "present working directory: $PWD"
 echo "home directory of current user: $HOME"
 echo "which user is running this script: $USER"
 echo "process id of current script: $$"
-sleep 60
+sleep 60 &
 echo "process id of last command in background: $!"
---
+++ 09-Condition.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+NUM=$1
+
+echo "Please enter the number :$1"
+
+if[ $NUM -gt 100]
+then
+    echo "Given number is greater than 100"
+else
+    echo "Given number is less than or equal to 100"
+fi    
