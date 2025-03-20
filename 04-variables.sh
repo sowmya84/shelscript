4638bc326c161fb45566f9019b2e4b709467bfdf
Sowmya Kancharla <vippasowmya0408@gmail.com>
2025-03-20 19:09:03 +0530

    Datatypes sum

--- 04-variables.sh
+++ 04-variables.sh
@@ -3,6 +3,8 @@
 PERSON1=$1 # No space before and after equal 
 PERSON2=$2
 
+#pass args while running
+
 echo "$PERSON1:: Hi $PERSON2"
 echo "$PERSON2:: Hello $PERSON1"
 echo "$PERSON1:: How are you doing?"
--- 05-variables.sh
+++ 05-variables.sh
@@ -6,6 +6,8 @@ read -s USERNAME
 
 echo "Usename entered is : $USERNAME"
 
+echo "Please enter your password::"
+
 read -s PASSWORD
 
 
