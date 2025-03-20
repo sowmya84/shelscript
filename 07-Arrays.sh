f01ba059450038bcc8181e1a1f27a7e838858614
Sowmya Kancharla <vippasowmya0408@gmail.com>
2025-03-20 19:21:29 +0530

    Array script

---
+++ 07-Arrays.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+MOVIES=("pushpa" "RRR" "Devara")
+
+echo "First movie: ${MOVIES[0]}"
+echo "Second movie: ${MOVIES[1]}"
+echo "Third movie: ${MOVIES[2]}"
+
+echo "All movies are: ${MOVIES[@]}"
