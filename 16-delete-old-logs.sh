18c520e938d28b5aebfb55cbe6044daf690a1f52
Sowmya Kancharla <vippasowmya0408@gmail.com>
2025-03-27 19:18:43 +0530

    16-delete-logs

--- 16-delete-old-logs.sh
+++ 16-delete-old-logs.sh
@@ -36,5 +36,9 @@ echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
 FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -mtime +14)
 echo "Files to be deleted: $FILES_TO_DELETE"
 
-
+while read -r 
+do
+    
+     echo "Deleting file: $file"
+done <<< $FILES_TO_DELETE
 
