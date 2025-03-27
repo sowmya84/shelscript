995bec1c7515f704a4ee909f77c3a1c61ddf3ec2
Sowmya Kancharla <vippasowmya0408@gmail.com>
2025-03-27 19:15:24 +0530

    sh 17-while.sh

--- 16-delete-old-logs.sh
+++ 16-delete-old-logs.sh
@@ -36,3 +36,5 @@ echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
 FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -mtime +14)
 echo "Files to be deleted: $FILES_TO_DELETE"
 
+
+
---
+++ 17-while.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+whilee read -r line
+do 
+    echo $line
+done < 16-delete-old-logs.sh
