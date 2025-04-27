d23abf00bedc5d328b387c260facb71d6335d79e
Sowmya Kancharla <vippasowmya0408@gmail.com>
2025-04-27 12:06:21 +0530

    Added backup

--- 16-delete-old-logs.sh
+++ 16-delete-old-logs.sh
@@ -33,7 +33,7 @@ CHECK_ROOT(){
 
 echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
 
-FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -mtime +14)
+FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -mtime +14) 
 echo "Files to be deleted: $FILES_TO_DELETE"
 
 while read -r file
---
+++ 18-backup.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+SOURCE_DIR=$1
+DEST_DIR=$2
+DAYS=${3:-14} # if user is not providing no of days we are taking 14 as default
+
+LOGS_FOLDER="/home/ec2-user/shellscript-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1 )
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
+
+USAGE() {
+    echo -e "$R USAGE:: $N sh 18-backup.sh <SOURCE_DIR> <DEST_DIR> <DAYS(Optional)>"
+}
+
+if [ $# -lt 2]
+then
+    USAGE
+fi
+
+echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
+
+# FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -mtime +14) 
+# echo "Files to be deleted: $FILES_TO_DELETE"
+
+# while read -r file
+# do
+    
+#      echo "Deleting file: $file"
+#     rm -rf $file
+# done <<< $FILES_TO_DELETE
+
