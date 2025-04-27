42910fc5490d5d38fa1cfcf2c97ed926435b8f32
Sowmya Kancharla <vippasowmya0408@gmail.com>
2025-04-27 12:19:48 +0530

    Added backup file

--- 18-backup.sh
+++ 18-backup.sh
@@ -27,8 +27,10 @@ VALIDATE(){
 
 USAGE() {
     echo -e "$R USAGE:: $N sh 18-backup.sh <SOURCE_DIR> <DEST_DIR> <DAYS(Optional)>"
+    exit 1
 }
-
+ mkdir -p /home/ec2-user/shellscript-logs/
+ 
 if [ $# -lt 2 ]
 then
     USAGE
